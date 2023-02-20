9067178c68abd78e10b88291f7de6d08bc7d7a69
Nijen83 <Nicolaj83@live.dk>
2023-02-20 12:44:35 +0100

    images

---
+++ src/main/java/com/example/oceanRescue/OceanRescueApplication.java
@@ -0,0 +1,13 @@
+package com.example.oceanRescue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanRescueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanRescueApplication.class, args);
+	}
+
+}
