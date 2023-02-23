da5bdeda88926114f700a389ca69c41c7ca5f7b3
Hauxy17 <ella.costello@yahoo.com>
2023-02-23 12:24:54 +0100

    Fixed our controller so that we can access our localhost / CSS / HTML from directly within the browser

--- src/test/java/com/example/oceanRescue/OceanRescueApplicationTests.java
+++ src/test/java/com/example/oceanRescue/OceanRescueApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.oceanRescue;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 @SpringBootTest
 class OceanRescueApplicationTests {
 
+	@Autowired
+	MockMvc mvc;
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void testString() throws Exception {
+
+		mvc.perform(MockMvcRequestBuilders.get("/"));
+	}
+
 
 }
