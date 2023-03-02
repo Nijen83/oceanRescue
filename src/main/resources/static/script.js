23e49a7343845af86960c2f70b4ed3714836d692
Nijen83 <nicolaj83@live.dk>
2023-03-02 09:33:51 +0100

    js comments + interval

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -15,42 +15,35 @@ const gameOver = document.querySelector(".gameOver");
 const yourScore = document.querySelector(".your_score");
 const life = document.querySelector(".life");
 
-var intervalFishLeft;
-var intervalFishRight;
-
 let score = 0;
 let healthCount = 10;
 let trashInterval = 2100;
+let fishInterval = 2500;
 
+//......Start click functions......
 function startGame(){
     msgArea.innerHTML = "";
     msgArea.classList.add("hidden");
     music.play();
-    addfishLeft();
-    addfishRight();
 
-    setInterval(addfishLeft, 6000);
-    setInterval(addfishRight, 6000);
+    setInterval(addfishLeft, fishInterval);
+    setInterval(addfishRight, fishInterval);
     setInterval(addTrash, trashInterval);
     updateScore();
     changeBackground();
-
-    var rand = Math.floor(Math.random() * 8000);
-    intervalFishLeft = setInterval(addfishLeft, rand);
-    intervalFishRight = setInterval(addfishRight, rand);
-    setInterval(addTrash, 8000);
-    updateScore();
-    changeBackground();
 }
+
 //......Display .rules div......
 function showRules(){
     rules.style.display = "block";
 }
+
 //......Display .scoreboard div......
 function displayHighscore(){
     endGame.innerHTML = score;
 }
-//......Change background image......
+
+//......Change background image + end of game......
 function changeBackground(){
     if(healthCount <= 5){
         topImage.classList.add("transparent");
@@ -66,23 +59,27 @@ function changeBackground(){
         endGame();
     }
 }
-
+//......Display .scoreboard div......
 function showScoreboard() {
     scoreboard.style.display = "block";
 }
-
+//......Adds fish from left......
 function addfishLeft(){
+
+//......Array of fish classes......
     var arr = ["fish_4_r", "fish_4", "fish_5", "seahorse", "fish_4", "stingray", "fish_7", "octopus", "fish_12_l", "fish_13_r", "fish_13_r", "fish_14_r"];
 
+//......Creates div + adds random class from array + styling......
     const fishLeft = document.createElement("div");
     fishLeft.classList.add(arr[Math.floor(Math.random() * arr.length)]);
     fishLeft.style.position = "absolute";
     fishLeft.style.left = "-200px";
     fishLeft.style.top = Math.floor(Math.random() * 700) + "px";
-    fishLeft.appendChild(document.createElement("div"));
 
+    fishLeft.appendChild(document.createElement("div"));
     gameArea.appendChild(fishLeft);
 
+//......Fish click functions......
     fishLeft.addEventListener('click', (event)=> {
         event.target.remove();
         healthCount--;
@@ -91,18 +88,23 @@ function addfishLeft(){
     });
 }
 
+//......Adds fish from right......
 function addfishRight(){
+
+//......Array of fish classes......
     var arr = ["fish_1", "fish_2", "fish_3", "fish_6", "fish_8", "turtle", "fish_1", "fish_9_r", "fish_10_r", "fish_10_r", "fish_10_r", "fish_11_r", "shrimp", "shrimp", "octopus_2", "seahorse_2", "seahorse_2"];
 
+//......Creates div + adds random class from array + styling......
     const fishRight = document.createElement("div");
     fishRight.classList.add(arr[Math.floor(Math.random() * arr.length)]);
     fishRight.style.position = "absolute";
     fishRight.style.left = "1200px";
     fishRight.style.top = Math.floor(Math.random() * 700) + "px";
-    fishRight.appendChild(document.createElement("div"));
 
+    fishRight.appendChild(document.createElement("div"));
     gameArea.appendChild(fishRight);
 
+//......Fish click functions......
     fishRight.addEventListener('click', (event)=> {
         event.target.remove();
         healthCount--;
@@ -110,22 +112,27 @@ function addfishRight(){
         changeBackground();
    });
 }
-
+//......Adds trash from top......
 function addTrash(){
+
+//......Array of trash classes......
     var arr = ["trash_1", "trash_2", "trash_3", "trash_4", "trash_5", "trash_6", "trash_7", "trash_8"];
 
+//......Creates div + adds random class from array + styling......
     const trash = document.createElement("div");
     trash.classList.add(arr[Math.floor(Math.random() * arr.length)]);
     trash.style.position = "absolute";
     trash.style.left = Math.floor(Math.random() *  1100) + "px";
     trash.style.top = "-100px";
-    trash.appendChild(document.createElement("div"));
 
+    trash.appendChild(document.createElement("div"));
     gameArea.appendChild(trash);
 
+//......trash click functions......
     trash.addEventListener('click', (event)=> {
         event.target.remove();
 
+//......Checking trash for number of points awarded......
         if(trash.getAttribute('class') === "trash_5" || trash.getAttribute('class') === "trash_6") {
             score += 10;
         } else if(trash.getAttribute('class') === "trash_7" || trash.getAttribute('class') === "trash_8") {
@@ -139,10 +146,12 @@ function addTrash(){
     });
 }
 
+//......Displaying + updating score & health......
 function updateScore(){
     title.innerHTML = "<span>Score: " + score +  " </br><img src='images/life.png' style='height: 30px; width: 30px; right: 50px; position: absolute;'> " + healthCount + "</span>";
 }
 
+//......Playing + muting background music......
 function mute() {
     if (!music.paused) {
       music.pause();
@@ -151,38 +160,46 @@ function mute() {
     }
 }
 
+//......Displaying .gameOver div + final score......
 function endGame() {
     gameOver.style.display = "block";
     document.getElementById("myHighscore").value = score;
     yourScore.innerHTML = "Your score: " + score;
 }
 
+//......Updating trashInterval......
 function levels() {
     if(score >= 500){
         trashInterval = 1800;
-    }
+        fishInterval = 2500;
+    }else
     if(score >= 1000){
         trashInterval = 1500;
-    }
+        fishInterval = 2000;
+    }else
     if(score >= 1500){
         trashInterval = 1200;
-    }
+        fishInterval = 1500;
+    }else
     if(score >= 2000){
         trashInterval = 900;
-    }
+        fishInterval = 1000;
+    }else
     if(score >= 2500){
         trashInterval = 600;
     }
 }
 
+//......Making bubbles on click......
 gameArea.addEventListener("click", e => {
+//......Position of cursor......
     const x = e.pageX;
     const y = e.pageY;
+//......Random size of bubble......
     const randomSize = Math.random() * (30 - 5) + 5;
-
+//......Creating div + adding .bubble......
     const bubble = document.createElement("div");
     bubble.classList.add('bubble');
-
     bubble.style.left = (x - 392) + "px";
     bubble.style.top = (y - 72) + "px";
     bubble.style.width = randomSize + "px";
@@ -190,8 +207,8 @@ gameArea.addEventListener("click", e => {
 
     gameArea.appendChild(bubble);
 
+//......Making bubbles hidden after X seconds......
     setTimeout(function(){
         bubble.style.display = 'none';
     }, 7000);
-
 });
